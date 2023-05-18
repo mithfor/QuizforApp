3b7949aa818ed897d7540b45d94e802962389f0f
Mithfor <mithfor@gmail.com>
2023-05-18 08:45:22 +0300

    Initial Commit

---
+++ QuizforApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizforApp
+//
+//  Created by Dmitrii Voronin on 18.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
