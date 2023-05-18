59bb093d1a125c0fd66c36779dbabb43fe8e4ff4
Mithfor <mithfor@gmail.com>
2023-05-18 11:11:10 +0300

    Testing TableView. Refactoring

--- QuizforApp/QuestionViewControler.swift
+++ QuizforApp/QuestionViewControler.swift
@@ -12,8 +12,9 @@ class QuestionViewControler: UIViewController {
     
     @IBOutlet weak var headerLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
-    private var question: String = ""
-    private var options: [String] = []
+    private var question = ""
+    private var options = [String]()
+    private let reuseIdentifier = "Cell"
     
     convenience init(question: String, options: [String]) {
         self.init()
@@ -25,17 +26,22 @@ class QuestionViewControler: UIViewController {
         super.viewDidLoad()
         
         headerLabel.text = question
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+    }
+    
+    private func dequeueCell(in tableVew: UITableView) -> UITableViewCell {
+        if let cell = tableVew.dequeueReusableCell(withIdentifier: reuseIdentifier) {
+            return cell
+        }
+        
+        return UITableViewCell(style: .default, reuseIdentifier: reuseIdentifier)
     }
 }
 
 extension QuestionViewControler: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
-//        var content = cell.defaultContentConfiguration()
-//        content.text = options[indexPath.row]
-//        cell.contentConfiguration = content
+        let cell = dequeueCell(in: tableView)
         
         cell.textLabel?.text = options[indexPath.row]
         
