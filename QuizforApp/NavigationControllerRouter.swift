9f6380aa14f2e0e5224548d8f65988ac24767ff0
Dmitriy Voronin <devoronin@gmail.com>
2023-06-30 09:18:47 +0300

    Added Question Enum

--- QuizforApp/NavigationControllerRouter.swift
+++ QuizforApp/NavigationControllerRouter.swift
@@ -8,32 +8,6 @@
 import UIKit
 import QuizforEngine
 
-enum Question<T: Hashable>: Hashable {
-    case singleAnswer(T)
-    case multipleAnswer(T)
-
-    func hash(into hasher: inout Hasher) {
-
-        switch self {
-        case .singleAnswer(let value):
-            hasher.combine(value)
-
-        case .multipleAnswer(let value):
-            hasher.combine(value)
-        }
-    }
-
-    static func == (lhs: Question<T>, rhs: Question<T>) -> Bool {
-        switch (lhs, rhs) {
-        case ( .singleAnswer(let aValue), .singleAnswer(let bValue) ):
-            return aValue == bValue
-        case ( .multipleAnswer(let aValue), .multipleAnswer(let bValue) ):
-            return aValue == bValue
-        default:
-            return false
-        }
-    }
-}
 
 protocol ViewControllerFactory {
     func questionViewController(for question: String, answerCallback: @escaping (String) -> Void) -> UIViewController
