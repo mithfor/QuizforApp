2892513be49da12ddf4914fa7e85af78cf3d1430
Dmitriy Voronin <devoronin@gmail.com>
2023-07-03 11:13:23 +0300

    Implementing and Testing ResultsPresenter

--- QuizforApp/ResultsPresenter.swift
+++ QuizforApp/ResultsPresenter.swift
@@ -8,11 +8,55 @@
 import Foundation
 import QuizforEngine
 
+// swiftlint:disable force_cast
+
 struct ResultsPresenter {
 
     let result: QuizResult<Question<String>, [String]>
+    let correctAnswers: [Question<String>: [String]]
 
     var summary: String {
         return "You got \(result.score)/\(result.answers.count) correct"
     }
+
+    var presentableAnswers: [PresentableAnswer] {
+        return result.answers.map { (question, userAnswer) in
+
+            guard let correctAnswer = correctAnswers[question] else {
+                fatalError("Couldn't find correct answer fo question: \(question)")
+            }
+
+            return presentableAnswer(question, userAnswer, correctAnswer)
+
+        }
+    }
+
+    private func presentableAnswer(_ question: Question<String>,
+                                   _ userAnswer: [String],
+                                   _ correctAnswer: [String]) -> PresentableAnswer {
+
+        switch question {
+        case .singleAnswer(let value), .multipleAnswer(let value):
+            return PresentableAnswer(question: value,
+                                     answer: formattedAnswer(correctAnswer),
+                                     wrongAnswer: formattedWrongAnswer(question,
+                                                              userAnswer,
+                                                              correctAnswer))
+
+        }
+
+    }
+
+    private func formattedAnswer(_ answer: [String]) -> String {
+        return answer.joined(separator: ", ")
+    }
+
+    private func formattedWrongAnswer(_ question: Question<String>,
+                             _ userAnswer: [String],
+                             _ correctAnswer: [String]) -> String? {
+        return correctAnswer == userAnswer ? nil : formattedAnswer(userAnswer)
+    }
+
 }
+
+// swiftlint:enable force_cast
--- QuizforAppTests/ResultsPresenterTest.swift
+++ QuizforAppTests/ResultsPresenterTest.swift
@@ -15,10 +15,71 @@ class ResultsPresenterTest: XCTestCase {
         // given (Arrange)
         let answers = [Question.singleAnswer("Q1"): ["A1"], Question.multipleAnswer("Q2"): ["A2", "A3"]]
         let result = QuizResult(answers: answers, score: 1)
-        let sut = ResultsPresenter(result: result)
+        let sut = ResultsPresenter(result: result, correctAnswers: [:])
         // when (Act)
 
         // then (Assert)
         XCTAssertEqual(sut.summary, "You got 1/2 correct")
     }
+
+    func test_presentableAnswers_withoutQuestions_isEmpty() {
+
+        let answers = [Question<String>: [String]]()
+        let result = QuizResult(answers: answers, score: 1)
+        let sut = ResultsPresenter(result: result, correctAnswers: [:])
+
+        XCTAssertTrue(sut.presentableAnswers.isEmpty)
+    }
+
+    func test_presentableAnswers_withWrongSingleAnswer_mapsAnswer() {
+        let answers = [Question.singleAnswer("Q1"): ["A1"]]
+        let correctAnswers = [Question.singleAnswer("Q1"): ["A2"]]
+        let result = QuizResult(answers: answers, score: 0)
+
+        let sut = ResultsPresenter(result: result, correctAnswers: correctAnswers)
+
+        XCTAssertEqual(sut.presentableAnswers.count, 1)
+        XCTAssertEqual(sut.presentableAnswers.first?.question, "Q1")
+        XCTAssertEqual(sut.presentableAnswers.first?.answer, "A2")
+        XCTAssertEqual(sut.presentableAnswers.first?.wrongAnswer, "A1")
+   }
+
+    func test_presentableAnswers_withWrongMultipleAnswers_mapsAnswer() {
+        let answers = [Question.multipleAnswer("Q1"): ["A1", "A4"]]
+        let correctAnswers = [Question.multipleAnswer("Q1"): ["A2", "A3"]]
+        let result = QuizResult(answers: answers, score: 0)
+
+        let sut = ResultsPresenter(result: result, correctAnswers: correctAnswers)
+
+        XCTAssertEqual(sut.presentableAnswers.count, 1)
+        XCTAssertEqual(sut.presentableAnswers.first?.question, "Q1")
+        XCTAssertEqual(sut.presentableAnswers.first?.answer, "A2, A3")
+        XCTAssertEqual(sut.presentableAnswers.first?.wrongAnswer, "A1, A4")
+   }
+
+    func test_presentableAnswers_withRightSingleAnswer_mapsAnswer() {
+        let answers = [Question.singleAnswer("Q1"): ["A1"]]
+        let correctAnswers = [Question.singleAnswer("Q1"): ["A1"]]
+        let result = QuizResult(answers: answers, score: 1)
+
+        let sut = ResultsPresenter(result: result, correctAnswers: correctAnswers)
+
+        XCTAssertEqual(sut.presentableAnswers.count, 1)
+        XCTAssertEqual(sut.presentableAnswers.first?.question, "Q1")
+        XCTAssertEqual(sut.presentableAnswers.first?.answer, "A1")
+        XCTAssertNil(sut.presentableAnswers.first?.wrongAnswer)
+   }
+
+    func test_presentableAnswers_withRightMultipleAnswers_mapsAnswer() {
+        let answers = [Question.multipleAnswer("Q1"): ["A1", "A4"]]
+        let correctAnswers = [Question.multipleAnswer("Q1"): ["A1", "A4"]]
+        let result = QuizResult(answers: answers, score: 0)
+
+        let sut = ResultsPresenter(result: result, correctAnswers: correctAnswers)
+
+        XCTAssertEqual(sut.presentableAnswers.count, 1)
+        XCTAssertEqual(sut.presentableAnswers.first?.question, "Q1")
+        XCTAssertEqual(sut.presentableAnswers.first?.answer, "A1, A4")
+        XCTAssertNil(sut.presentableAnswers.first?.wrongAnswer)
+   }
 }
