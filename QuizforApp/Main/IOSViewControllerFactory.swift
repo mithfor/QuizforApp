b973aa8c8f23ab4e3c52e88b49e305ac31dc403d
Dmitriy Voronin <devoronin@gmail.com>
2023-07-08 16:49:01 +0300

    Introduced the new factory initializer

--- QuizforApp/Main/IOSViewControllerFactory.swift
+++ QuizforApp/Main/IOSViewControllerFactory.swift
@@ -10,14 +10,25 @@ import QuizforEngine
 
 class IOSViewControllerFactory: ViewControllerFactory {
 
+    typealias Answers = [(question: Question<String>, answers: [String])]
+
     private let questions: [Question<String>]
     private let options: [Question<String>: [String]]
-    private let correctAnswers: [Question<String>: [String]]
+    private let correctAnswers: () -> Answers
+
+    init(options: [Question<String>: [String]], correctAnswers: Answers) {
+        self.questions = correctAnswers.map { $0.question }
+        self.options = options
+        self.correctAnswers = { correctAnswers }
+    }
 
     init(questions: [Question<String>], options: [Question<String>: [String]], correctAnswers: [Question<String>: [String]]) {
         self.questions = questions
         self.options = options
-        self.correctAnswers = correctAnswers
+        self.correctAnswers = { questions.map { question in
+                (question, correctAnswers[question]!)
+            }
+        }
     }
 
     func questionViewController(for question: Question<String>,
@@ -70,7 +81,7 @@ class IOSViewControllerFactory: ViewControllerFactory {
     func resultViewController(for result: QuizforEngine.Result<Question<String>, [String]>) -> UIViewController {
         
         let presenter = ResultsPresenter(userAnswers: questions.map { question in (question, result.answers[question]!)},
-                                         correctAnswers: questions.map { question in (question, correctAnswers[question]!)},
+                                         correctAnswers: correctAnswers(),
                                          scorer: { _, _ in result.score})
 
         let controller =  ResultsViewController(summary: presenter.summary,
