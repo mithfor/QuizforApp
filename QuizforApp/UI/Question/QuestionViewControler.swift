dcb2e349d6015cc37349b17b5829aedbd0b56d38
Dmitriy Voronin <devoronin@gmail.com>
2023-07-25 05:22:21 +0300

    Fix Results View controller, QuestionViewController, AnswerCells xib target issues

--- QuizforApp/UI/Question/QuestionViewControler.swift
+++ QuizforApp/UI/Question/QuestionViewControler.swift
@@ -27,7 +27,6 @@ class QuestionViewControler: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         tableView.allowsMultipleSelection = allowsMultipleSelection
         headerLabel.text = question
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
