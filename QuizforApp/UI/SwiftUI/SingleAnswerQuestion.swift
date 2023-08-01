762a60769b7758b16976b9e84ca0aac1aa389836
Dmitriy Voronin <devoronin@gmail.com>
2023-08-01 21:58:14 +0300

    Add SingleAnswerQuestion

---
+++ QuizforApp/UI/SwiftUI/SingleAnswerQuestion.swift
@@ -0,0 +1,76 @@
+//
+//  SingleAnswerQuestion.swift
+//  QuizforApp
+//
+//  Created by Dmitriy Voronin on 01.08.2023.
+//
+
+import SwiftUI
+
+struct SingleAnswerQuestion: View {
+    let title: String
+    let question: String
+    let options: [String]
+    let selection: (String) -> Void
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0.0) {
+            VStack(alignment: .leading, spacing: 16) {
+                Text(title)
+                    .font(.headline)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.blue)
+                    .padding(.top)
+
+                Text(question)
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+            }.padding()
+
+            ForEach(options, id: \.self) { option in
+                Button(action: {}, label: {
+                    HStack {
+                        Circle()
+                            .stroke(Color.secondary, lineWidth: 2.5)
+                            .frame(width: 40.0, height: 40.0)
+                        Text(option)
+                            .font(.title)
+                            .foregroundColor(Color.secondary)
+                        Spacer()
+                    }.padding()
+
+                })
+            }
+
+            Spacer()
+        }
+    }
+}
+
+struct SingleAnswerQuestion_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            SingleAnswerQuestion(
+                title: "1 of 2",
+                question: "What is the fifth planet from the sun?",
+                options: [
+                    "Venus",
+                    "Jupiter",
+                    "Earth",
+                    "Pluto"
+                ],
+                selection: { _ in })
+
+            SingleAnswerQuestion(
+                title: "1 of 2",
+                question: "What is the fifth planet from the sun?",
+                options: [
+                    "Venus",
+                    "Jupiter",
+                    "Earth",
+                    "Pluto"
+                ],
+                selection: { _ in }).preferredColorScheme(.dark)
+        }
+    }
+}
