0cbc731308df5d001ade72038279f276f9a67903
Dmitriy Voronin <devoronin@gmail.com>
2023-08-02 08:05:09 +0300

    Toggle selection state

---
+++ QuizforAppTests/UI/SwiftUI/MultipleSelectionStoreTest.swift
@@ -0,0 +1,42 @@
+//
+//  MultipleSelectionStoreTest.swift
+//  QuizforAppTests
+//
+//  Created by Dmitriy Voronin on 02.08.2023.
+//
+
+import XCTest
+
+struct MultipleSelectionStore {
+
+    var options: [MultipleSelectionOption]
+
+    internal init(options: [String]) {
+        self.options = options.map { MultipleSelectionOption(text: $0) }
+    }
+}
+
+struct MultipleSelectionOption {
+    let text: String
+    var isSelected = false
+
+    mutating func select() {
+        isSelected.toggle()
+    }
+}
+
+final class MultipleSelectionStoreTest: XCTestCase {
+
+    func testExample() {
+        var sut = MultipleSelectionStore(options: ["o0", "o1"])
+        XCTAssertFalse(sut.options[0].isSelected)
+
+        sut.options[0].select()
+        XCTAssertTrue(sut.options[0].isSelected)
+
+        sut.options[0].select()
+        XCTAssertFalse(sut.options[0].isSelected)
+    }
+
+
+}
