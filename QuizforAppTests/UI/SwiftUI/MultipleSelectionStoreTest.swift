2281f649468b9c255ca930ec005cb4d40df20918
Dmitriy Voronin <devoronin@gmail.com>
2023-08-02 08:58:43 +0300

    Move MultipleSelectionStore to production codebase

--- QuizforAppTests/UI/SwiftUI/MultipleSelectionStoreTest.swift
+++ QuizforAppTests/UI/SwiftUI/MultipleSelectionStoreTest.swift
@@ -6,37 +6,7 @@
 //
 
 import XCTest
-
-struct MultipleSelectionStore {
-
-    var options: [MultipleSelectionOption]
-    var canSubmit: Bool {
-
-        !options.filter(\.isSelected).isEmpty
-    }
-
-    private let handler: ([String]) -> Void
-
-    internal init(options: [String], handler: @escaping ([String]) -> Void = { _ in }) {
-        self.options = options.map { MultipleSelectionOption(text: $0) }
-        self.handler = handler
-    }
-
-    func submit() {
-        guard canSubmit else { return }
-        handler(options.filter(\.isSelected).map(\.text))
-    }
-}
-
-struct MultipleSelectionOption {
-    let text: String
-    var isSelected = false
-
-
-    mutating func select() {
-        isSelected.toggle()
-    }
-}
+@testable import QuizforApp
 
 final class MultipleSelectionStoreTest: XCTestCase {
 
