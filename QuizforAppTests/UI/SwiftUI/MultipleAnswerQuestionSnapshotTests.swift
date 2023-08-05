6ed21ff3f6d8b6347948e51f766b73862dbcf4a3
Dmitriy Voronin <devoronin@gmail.com>
2023-08-05 14:45:34 +0300

    WIP applying snapshotTests

---
+++ QuizforAppTests/UI/SwiftUI/MultipleAnswerQuestionSnapshotTests.swift
@@ -0,0 +1,21 @@
+//
+//  MultipleAnswerQuestionSnapshotTests.swift
+//  QuizforAppTests
+//
+//  Created by Dmitriy Voronin on 02.08.2023.
+//
+
+import XCTest
+@testable import QuizforApp
+
+final class MultipleAnswerQuestionSnapshotTests: XCTestCase {
+
+//    func testExample() {
+//        let sut = MultipleAnswerQuestion(
+//            title: "A title",
+//            question: "A question",
+//            store: .init(options: ["Option 1", "Option 2"]))
+//
+//        record(snapshot: sut.snapshot(for: <#SnapshotConfiguration#>), named: "two_options")
+//    }
+}
